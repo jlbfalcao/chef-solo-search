2da926dd69c219bf15cf60306c495faa6b6cb178
Jorge Falcão <jlbfalcao@gmail.com>
2012-01-21 08:14:49 -0200

    changing metadata doc file

--- metadata.rb
+++ metadata.rb
@@ -2,5 +2,5 @@ maintainer       "Markus Korn"
 maintainer_email "thekorn@gmx.de"
 # license          ""
 description      "Data bag search for Chef Solo"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1"
